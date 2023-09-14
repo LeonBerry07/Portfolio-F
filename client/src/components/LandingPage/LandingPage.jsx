157ca6ddd646b1665086bab1fdaf942fcfdebe19
LeonBerry07 <altairconnor12@gmail.com>
2023-09-14 14:49:25 -0300

    First-Lap-Finished

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,48 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link as RouterLink } from 'react-router-dom';
+import { Link } from "react-scroll";
 import'./LandingPage.css'
 
 const url12 = 'me.jpg';
 // const url13 = 'distopya.jpg';
+const gmail = 'gmail.png';
+const copy = 'portap.png';
+const lin = '174857.png';
+const dealup = 'https://start-bussines.vercel.app/';
 {/* <img className="bru" src={url13} alt="Food's Lyon"/> */}
 {/* <img className="bru" src={url12} alt="Food's Lyon"/> */}
 
 class LandingPage extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.emailRef = React.createRef();
+    }
+
+    copiarEmailAlPortapapeles = () => {
+        const emailText = this.emailRef.current.innerText;
+
+        // Crear un campo de texto oculto
+        const textArea = document.createElement('textarea');
+        textArea.value = emailText;
+
+        // Agregar el campo de texto oculto al documento
+        document.body.appendChild(textArea);
+
+        // Seleccionar el texto en el campo de texto
+        textArea.select();
+
+        // Intentar copiar el texto al portapapeles
+        try {
+            document.execCommand('copy');
+            alert('¡Dirección de correo copiada al portapapeles!');
+        } catch (err) {
+            console.error('Error al copiar al portapapeles: ', err);
+        }
+
+        // Eliminar el campo de texto oculto
+        document.body.removeChild(textArea);
+    };
 
     render() {
         return(
@@ -18,9 +53,23 @@ class LandingPage extends React.Component{
                 
             
             <header className="top">
-                <p>About</p>
-                <p>Skills</p>
-                <p>Contact</p>
+
+                <p>
+                <Link to="about" smooth={true} duration={500}>About</Link>
+                </p>
+
+                <p>
+                <Link to="skills" smooth={true} duration={500}>Skills</Link>
+                </p>
+
+                <p>
+                <Link to="projects" smooth={true} duration={500}>Projects</Link>
+                </p>
+
+                <p>
+                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                </p>
+
             </header>
 
 
@@ -42,7 +91,26 @@ class LandingPage extends React.Component{
 
 
 
-            <section className="title">About Me</section>
+            <section id="about" className="title">About Me</section>
+
+            <div className="about">
+
+            <p>
+                Hi, i am Bruno Leonel Mendiberry 👋
+            </p>
+
+            <p className="text">
+                I work as a Fullstack Developer oriented to Back-End, and with knowledge of photo editing.
+            </p>
+
+            <p>    
+                I am a lifelong lover of technology and computing, both from the hardware and software side.
+            </p>
+
+            <p>
+                Currently I develop projects that allow me to grow as a programmer, both from the technical and creative side. These projects are related to my personal tastes and ideas that I think need to be implemented in society.
+            </p>
+            </div>
 
             <div className="about">
 
@@ -66,7 +134,7 @@ class LandingPage extends React.Component{
 
 
 
-            <section className="title">Skills</section>
+            <section id="skills" className="title">Skills</section>
             
             <div className="skills">
 
@@ -119,13 +187,13 @@ class LandingPage extends React.Component{
 
 
 
-            <section className="title">Projects</section>
+            <section id="projects" className="title">Projects</section>
 
             <div className="boxes">
                 
 
             <div className="box">
-                Portfolio   
+                Lyon Berry (En construcción) 
             </div>
 
             <div className="box">
@@ -133,15 +201,15 @@ class LandingPage extends React.Component{
             </div>
 
             <div className="box">
-                Deal Up!
+                <button className='box'>
+                <RouterLink to='https://start-bussines.vercel.app/'>Deal Up!</RouterLink>
+                </button>
             </div>
             
             </div>
             
 
-            {/* <button className='home'>
-                <Link to="/home">Projects</Link>
-            </button> */}
+            
 
 
 
@@ -151,18 +219,27 @@ class LandingPage extends React.Component{
 
 
 
-            <section className="title">Contact</section>
+            <section id="contact" className="title">Contact</section>
 
             <div className="contact">
-            <p>
-                E-Mail
+            <img className="lin" src={gmail} alt="Food's Lyon"/>
+
+            <p ref={this.emailRef}>
                 altairconnor12@gmail.com
             </p>
 
-            <p> 
-                LinkedIn
-                https://www.linkedin.com/in/bruno-leonel-a40110260/
-            </p>
+            <button className="email" onClick={this.copiarEmailAlPortapapeles}>
+                {/* <img className="lin" src={copy} alt="Food's Lyon"/> */}
+                Copy E-mail
+            </button>
+
+            </div>
+
+            <div className="contact">
+            <RouterLink to='https://www.linkedin.com/in/bruno-leonel-a40110260/'>
+                <img className="lin" src={lin} alt="Food's Lyon"/>
+            </RouterLink>
+            
             </div>
 
 
@@ -173,9 +250,21 @@ class LandingPage extends React.Component{
 
 
             <footer className="foot">
-                <p>About</p>
-                <p>Skills</p>
-                <p>Contact</p>
+                <p>
+                <Link to="about" smooth={true} duration={500}>About</Link>
+                </p>
+
+                <p>
+                <Link to="skills" smooth={true} duration={500}>Skills</Link>
+                </p>
+
+                <p>
+                <Link to="projects" smooth={true} duration={500}>Projects</Link>
+                </p>
+
+                <p>
+                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                </p>
                 <p>&copy;Bruno Leonel Mendiberry 2023</p>
             </footer>
 
